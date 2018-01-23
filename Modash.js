5300a343ce732e90194e10592911e46ca45498a4
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-01-23 11:56:24 -0500

    Writes out code to be tested.

--- Modash.js
+++ Modash.js
@@ -1,1 +1,17 @@
-// We write the Modash library in this file in the Unit Testing chapter
+// truncate(string, length)
+const s = 'All code and no tests makes Kenia a precarious cat.';
+Modash.truncate(s, 21);
+Modash.truncate(s, 100);
+
+
+// capitalize(string)
+const x = 'stability was practically ASSURED.';
+Modash.capitalize(s);
+
+
+//camelCase(string)
+let y = 'started at';
+Modash.camelCase(s);
+
+y = 'started_at';
+Modash.camelCase(s);
